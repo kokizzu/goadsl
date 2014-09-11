7a8e48f7fb1ede68042a31ceaddb040adad74750
judika03 <judikagultom03@gmail.com>
2014-09-11 05:08:01 -0700

    revisi

--- linkedlist.go
+++ linkedlist.go
@@ -1,162 +1,63 @@
-package list
+package main
 
-type Element struct {
-	next, prev *Element
+import "fmt"
 
-	list *List
+type Any interface{}
 
-	Value interface{}
+type Item struct { 
+    pNext *Item
+    val rune
 }
 
-func (e *Element) Next() *Element {
-	if p := e.next; e.list != nil && p != &e.list.root {
-		return p
-	}
-	return nil
+func createList() *Item {
+
+    pHead := &Item{nil, 'a'}
+    
+    pCurr := pHead
+    for i:= 'b'; i <= 'z'; i++ { 
+        pItem := &Item{nil, i}       
+        pCurr.pNext = pItem
+        pCurr = pItem
+    }
+    
+    return pHead
 }
 
-func (e *Element) Prev() *Element {
-	if p := e.prev; e.list != nil && p != &e.list.root {
-		return p
-	}
-	return nil
+func printList(pList *Item) {
+
+    pCurr := pList
+    for {
+        fmt.Printf("%c", pCurr.val)
+        
+        if pCurr.pNext != nil {
+            pCurr = pCurr.pNext
+        } else {
+            break
+        }
+    }
+    fmt.Println("")
 }
 
-type List struct {
-	root Element
-	len  int
+func reverseList(pList *Item) *Item {
+    
+    pCurr := pList
+    var pTop *Item = nil
+    for {
+        if pCurr == nil {
+            break
+        }
+        pTemp := pCurr.pNext
+        pCurr.pNext = pTop
+        pTop = pCurr
+        pCurr = pTemp        
+    }
+    
+    return pTop
 }
 
-func (l *List) Init() *List {
-	l.root.next = &l.root
-	l.root.prev = &l.root
-	l.len = 0
-	return l
-}
+func main() {
 
-func New() *List { return new(List).Init() }
-
-func (l *List) Len() int { return l.len }
-
-func (l *List) Front() *Element {
-	if l.len == 0 {
-		return nil
-	}
-	return l.root.next
-}
-
-func (l *List) Back() *Element {
-	if l.len == 0 {
-		return nil
-	}
-	return l.root.prev
-}
-
-func (l *List) lazyInit() {
-	if l.root.next == nil {
-		l.Init()
-	}
-}
-
-func (l *List) insert(e, at *Element) *Element {
-	n := at.next
-	at.next = e
-	e.prev = at
-	e.next = n
-	n.prev = e
-	e.list = l
-	l.len++
-	return e
-}
-
-func (l *List) insertValue(v interface{}, at *Element) *Element {
-	return l.insert(&Element{Value: v}, at)
-}
-
-func (l *List) remove(e *Element) *Element {
-	e.prev.next = e.next
-	e.next.prev = e.prev
-	e.next = nil
-	e.prev = nil
-	e.list = nil
-	l.len--
-	return e
-}
-
-func (l *List) Remove(e *Element) interface{} {
-	if e.list == l {
-
-		l.remove(e)
-	}
-	return e.Value
-}
-
-func (l *List) PushFront(v interface{}) *Element {
-	l.lazyInit()
-	return l.insertValue(v, &l.root)
-}
-
-func (l *List) PushBack(v interface{}) *Element {
-	l.lazyInit()
-	return l.insertValue(v, l.root.prev)
-}
-
-func (l *List) InsertBefore(v interface{}, mark *Element) *Element {
-	if mark.list != l {
-		return nil
-	}
-
-	return l.insertValue(v, mark.prev)
-}
-
-func (l *List) InsertAfter(v interface{}, mark *Element) *Element {
-	if mark.list != l {
-		return nil
-	}
-
-	return l.insertValue(v, mark)
-}
-
-func (l *List) MoveToFront(e *Element) {
-	if e.list != l || l.root.next == e {
-		return
-	}
-
-	l.insert(l.remove(e), &l.root)
-}
-
-func (l *List) MoveToBack(e *Element) {
-	if e.list != l || l.root.prev == e {
-		return
-	}
-
-	l.insert(l.remove(e), l.root.prev)
-}
-
-func (l *List) MoveBefore(e, mark *Element) {
-	if e.list != l || e == mark || mark.list != l {
-		return
-	}
-	l.insert(l.remove(e), mark.prev)
-}
-
-func (l *List) MoveAfter(e, mark *Element) {
-	if e.list != l || e == mark || mark.list != l {
-		return
-	}
-	l.insert(l.remove(e), mark)
-}
-
-func (l *List) PushBackList(other *List) {
-	l.lazyInit()
-	for i, e := other.Len(), other.Front(); i > 0; i, e = i-1, e.Next() {
-		l.insertValue(e.Value, l.root.prev)
-	}
-}
-
-func (l *List) PushFrontList(other *List) {
-	l.lazyInit()
-	for i, e := other.Len(), other.Back(); i > 0; i, e = i-1, e.Prev() {
-		l.insertValue(e.Value, &l.root)
-	}
+    var pList = createList() 
+    printList(pList)
+    printList(reverseList(pList))
 }
