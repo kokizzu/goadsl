104da2ca5f3de5ed39dc0b98e7782ce7bf20fa5c
judika03 <judikagultom03@gmail.com>
2014-09-11 05:35:51 -0700

    revisi

--- doublylinkedlist.go
+++ doublylinkedlist.go
@@ -1,23 +1,22 @@
 package main
-
 import (
-	"errors"
 	"fmt"
+	"errors"
 	"strings"
 )
 
+
 type Value struct {
-	Name      string
+	Name string
 	MilesAway int
 }
 type Node struct {
-	Value      // Embedded struct
-	next, prev *Node
+	Value				// Embedded struct
+	next, prev  *Node
 }
 type List struct {
 	head, tail *Node
 }
-
 func (l *List) First() *Node {
 	return l.head
 }
@@ -27,17 +26,16 @@ func (n *Node) Next() *Node {
 func (n *Node) Prev() *Node {
 	return n.prev
 }
-
 // Create new node with value
 func (l *List) Push(v Value) *List {
 	n := &Node{Value: v}
 	if l.head == nil {
-		l.head = n // First node
+		l.head = n		// First node
 	} else {
-		l.tail.next = n // Add after prev last node
+		l.tail.next = n	// Add after prev last node
 		n.prev = l.tail // Link back to prev last node
 	}
-	l.tail = n // reset tail to newly added node
+	l.tail = n  		// reset tail to newly added node
 	return l
 }
 func (l *List) Find(name string) *Node {
@@ -65,9 +63,7 @@ func (l *List) Delete(name string) bool {
 	}
 	return success
 }
-
 var errEmpty = errors.New("ERROR - List is empty")
-
 // Pop last item from list
 func (l *List) Pop() (v Value, err error) {
 	if l.tail == nil {
@@ -84,73 +80,70 @@ func (l *List) Pop() (v Value, err error) {
 
 func main() {
 	dashes := strings.Repeat("-", 50)
-	l := new(List) // Create Doubly Linked List
-
-	l.Push(Value{Name: "Atlanta", MilesAway: 0})
-	l.Push(Value{Name: "Las Vegas", MilesAway: 1961})
-	l.Push(Value{Name: "New York", MilesAway: 881})
+	l := new(List) 
+	l.Push(Value{Name: "Indonesia", MilesAway: 0})
+	l.Push(Value{Name: "Medan", MilesAway: 1961})
+	l.Push(Value{Name: "Kuala Namu", MilesAway: 881})
 
 	processed := make(map[*Node]bool)
 
-	fmt.Println("First time through list...")
+	fmt.Println("waktu pertama ")
 	for n := l.First(); n != nil; n = n.Next() {
 		fmt.Printf("%v\n", n.Value)
 		if processed[n] {
-			fmt.Printf("%s as been processed\n", n.Value)
+			fmt.Printf("%s di proses\n", n.Value)
 		}
 		processed[n] = true
 	}
 	fmt.Println(dashes)
-	fmt.Println("Second time through list...")
+	fmt.Println("waktu kedua")
 	for n := l.First(); n != nil; n = n.Next() {
 		fmt.Printf("%v", n.Value)
 		if processed[n] {
-			fmt.Println(" has been processed")
-		} else {
-			fmt.Println()
-		}
+			fmt.Println(" diproses")
+		} else { fmt.Println() }
 		processed[n] = true
 	}
 
 	fmt.Println(dashes)
 	var found_node *Node
-	city_to_find := "New York"
-	found_node = l.Find(city_to_find)
-	if found_node == nil {
-		fmt.Printf("NOT FOUND: %v\n", city_to_find)
+	kota_dicari := "Kuala namu"
+	found_node = l.Find(kota_dicari)
+	if found_node != nil {
+		fmt.Printf("tidak ditemukan: %v\n", kota_dicari)
 	} else {
-		fmt.Printf("FOUND: %v\n", city_to_find)
+		fmt.Printf("ditemukan: %v\n", kota_dicari)
 	}
 
-	city_to_find = "Chicago"
-	found_node = l.Find(city_to_find)
+	kota_dicari = "Pantai labu"
+	found_node = l.Find(kota_dicari)
 	if found_node == nil {
-		fmt.Printf("NOT FOUND: %v\n", city_to_find)
+		fmt.Printf("tidak ditemukan: %v\n", kota_dicari)
 	} else {
-		fmt.Printf("FOUND: %v\n", city_to_find)
+		fmt.Printf("ditemukan: %v\n", kota_dicari)
 	}
 
 	fmt.Println(dashes)
-	city_to_remove := "Las Vegas"
-	successfully_removed_city := l.Delete(city_to_remove)
+	kota_dihapus := "Siantar"
+	successfully_removed_city := l.Delete(	kota_dihapus)
 	if successfully_removed_city {
-		fmt.Printf("REMOVED: %v\n", city_to_remove)
+		fmt.Printf("dihapus: %v\n",kota_dihapus)
 	} else {
-		fmt.Printf("DID NOT REMOVE: %v\n", city_to_remove)
+		fmt.Printf("tidak dihapus: %v\n", 	kota_dihapus)
 	}
 
-	city_to_remove = "Chicago"
-	successfully_removed_city = l.Delete(city_to_remove)
+		kota_dihapus= "Pantai labu"
+	successfully_removed_city = l.Delete(	kota_dihapus)
 	if successfully_removed_city {
-		fmt.Printf("REMOVED: %v\n", city_to_remove)
+		fmt.Printf("dihapus: %v\n", 	kota_dihapus)
 	} else {
-		fmt.Printf("DID NOT REMOVE: %v\n", city_to_remove)
+		fmt.Printf("tidak dihapus: %v\n", 	kota_dihapus)
 	}
 
 	fmt.Println(dashes)
-	fmt.Println("* Pop each value off list...")
+	fmt.Println("Nilai")
 	for v, err := l.Pop(); err == nil; v, err = l.Pop() {
 		fmt.Printf("%v\n", v)
 	}
-	fmt.Println(l.Pop()) // Generate error - attempt to pop from empty list
+	fmt.Println(l.Pop())  
 }
